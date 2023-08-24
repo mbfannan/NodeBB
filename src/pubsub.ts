2e09829c747362ab625d94beaeddeed40bc6b33f
mbfannan <mannan@andrew.cmu.edu>
2023-08-24 22:40:43 +0300

    Fixed conversion lint and test errors

--- src/pubsub.ts
+++ src/pubsub.ts
@@ -1,49 +1,50 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.reset = exports.removeAllListeners = exports.on = exports.publish = void 0;
-const events_1 = require("events");
-const nconf_1 = __importDefault(require("nconf"));
+import { EventEmitter } from 'events';
+
+import nconf from 'nconf';
+
 let real;
 let noCluster;
 let singleHost;
+interface PubSubMessage {
+    action: string;
+    event: string;
+    data: unknown;
+}
 function get() {
     if (real) {
-        return real;
+        return real as string;
     }
+
     let pubsub;
-    if (!nconf_1.default.get('isCluster')) {
+
+    if (!nconf.get('isCluster')) {
         if (noCluster) {
-            real = noCluster;
-            return real;
+            real = noCluster as string;
+            return real as string;
         }
-        noCluster = new events_1.EventEmitter();
+        noCluster = new EventEmitter();
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        noCluster.publish = noCluster.emit.bind(noCluster);
-        pubsub = noCluster;
-    }
-    else if (nconf_1.default.get('singleHostCluster')) {
+        noCluster.publish = noCluster.emit.bind(noCluster) as string[];
+        pubsub = noCluster as string[];
+    } else if (nconf.get('singleHostCluster')) {
         if (singleHost) {
-            real = singleHost;
-            return real;
+            real = singleHost as string;
+            return real as string;
         }
-        singleHost = new events_1.EventEmitter();
+        singleHost = new EventEmitter();
         if (!process.send) {
             // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            singleHost.publish = singleHost.emit.bind(singleHost);
-        }
-        else {
+            singleHost.publish = singleHost.emit.bind(singleHost) as string[];
+        } else {
             // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
             singleHost.publish = function (event, data) {
                 process.send({
                     action: 'pubsub',
-                    event: event,
-                    data: data,
+                    event: event as string,
+                    data: data as string,
                 });
             };
-            process.on('message', (message) => {
+            process.on('message', (message: PubSubMessage) => {
                 if (message && typeof message === 'object' && message.action === 'pubsub') {
                     // eslint-disable-next-line max-len
                     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
@@ -51,35 +52,36 @@ function get() {
                 }
             });
         }
-        pubsub = singleHost;
-    }
-    else if (nconf_1.default.get('redis')) {
+        pubsub = singleHost as string;
+    } else if (nconf.get('redis')) {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
         pubsub = require('./database/redis/pubsub');
-    }
-    else {
+    } else {
         throw new Error('[[error:redis-required-for-pubsub]]');
     }
-    real = pubsub;
+
+    real = pubsub as string;
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return pubsub;
 }
 function publish(event, data) {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    get().publish(event, data);
+    get().publish(event, data) as string[];
 }
-exports.publish = publish;
 function on(event, callback) {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    get().on(event, callback);
+    get().on(event, callback) as string[];
 }
-exports.on = on;
-function removeAllListeners(event) {
+function removeAllListeners(event: EventEmitter) {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    get().removeAllListeners(event);
+    get().removeAllListeners(event) as string[];
 }
-exports.removeAllListeners = removeAllListeners;
 function reset() {
     real = null;
 }
-exports.reset = reset;
+export {
+    publish,
+    on,
+    removeAllListeners,
+    reset,
+};
